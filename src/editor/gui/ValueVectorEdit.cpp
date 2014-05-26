4f01a8dc6bf178b2c9f22358b467835e52e5b5d4
loafofpiecrust <taylorsnead@gmail.com>
2014-05-25 21:17:31 -0500

    Fix shit

---
+++ src/editor/gui/ValueVectorEdit.cpp
@@ -0,0 +1,14 @@
+#include "ValueVectorEdit.h"
+
+namespace lfant {
+namespace editor {
+namespace gui {
+
+ValueVectorEdit::ValueVectorEdit(QWidget *parent) :
+	QWidget(parent)
+{
+}
+
+} // namespace gui
+} // namespace editor
+} // namespace lfant
---
+++ src/editor/gui/ValueVectorEdit.h
@@ -0,0 +1,26 @@
+#ifndef LFANT_EDITOR_GUI_VALUEVECTOREDIT_H
+#define LFANT_EDITOR_GUI_VALUEVECTOREDIT_H
+
+#include <QWidget>
+
+namespace lfant {
+namespace editor {
+namespace gui {
+
+class ValueVectorEdit : public QWidget
+{
+	Q_OBJECT
+public:
+	explicit ValueVectorEdit(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+} // namespace gui
+} // namespace editor
+} // namespace lfant
+
+#endif // LFANT_EDITOR_GUI_VALUEVECTOREDIT_H
